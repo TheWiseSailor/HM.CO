de5fd903c2d003f82b0e3c8f6bafadc9e04e8dd4
TheWiseSailor <rudasilljay@gmail.com>
2024-02-18 17:28:06 -0500

    initiation/filechanges/homescreen beginning/ will be channing logo soon

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import HomeRender from './Components/HomeRender';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes> 
+          <Route path="/Holly.CO" element={<HomeRender />} />
+          <Route path="/" element={<HomeRender />} /> 
+
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;

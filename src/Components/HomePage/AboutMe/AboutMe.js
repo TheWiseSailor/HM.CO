37000126dbde62ef2305fe41dd8c8aaabeb07b17
TheWiseSailor <rudasilljay@gmail.com>
2024-02-20 11:58:00 -0500

    socials section

--- src/Components/HomePage/AboutMe/AboutMe.js
+++ src/Components/HomePage/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
       <div className="ABContainer">
         <h2 className='H2Header'>About Me</h2>
         <p className='AboutMeP'>
-I'm currenlty pursuing an education to become an interior designer, I love getting to customoize and make aesthetically pleasing home designs! I also love painting, and drawing when ever I have the chance!       </p>
+I'm currenlty pursuing an education to become an interior designer, I love getting to customize and make aesthetically pleasing home designs! I also love painting, and drawing when ever I have the chance!       </p>
       </div>
       <img src={Holly2} alt="Profile Picture" className="profile-picture" />
     </div>

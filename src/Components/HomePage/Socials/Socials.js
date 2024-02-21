6134dcaa3127dd5b28ffaddf5df06031ef8bf5a3
TheWiseSailor <rudasilljay@gmail.com>
2024-02-21 14:21:39 -0500

    Update

--- src/Components/HomePage/Socials/Socials.js
+++ src/Components/HomePage/Socials/Socials.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Socials.css';
 import mailIcon from './SocialImages/mail.png';
-import phoneIcon from './SocialImages/phone.png';
+
 import instagramIcon from './SocialImages/instagram.png';
 
 const Social = () => {
@@ -15,15 +15,13 @@ const Social = () => {
       <div className='SocialContactContainer' data-aos="fade-up" data-aos-duration="3000">
         <h2>Contact Me!</h2>
         <div className="social-icons">
-          <a href="" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/_.holllayyy/" target="_blank" rel="noopener noreferrer">
             <img src={instagramIcon} alt="Instagram" />
           </a>
-          <a href="mailto:" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:Hollylynne16@gmail.com" target="_blank" rel="noopener noreferrer">
             <img src={mailIcon} alt="Email" />
           </a>
-          <a href="tel:+" onClick={handleCall}>
-            <img src={phoneIcon} alt="Phone" />
-          </a>
+
         </div>
       </div>
     </div>
